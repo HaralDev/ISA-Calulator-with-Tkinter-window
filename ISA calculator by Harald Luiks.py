838e29087aafe70603b7ae79498020586891c7a4
Haraldeon <39010881+Haraldeon@users.noreply.github.com>
2018-05-08 20:47:01 +0200

    Update ISA calculator by Harald Luiks.py

--- ISA calculator by Harald Luiks.py
+++ ISA calculator by Harald Luiks.py
@@ -1,139 +1,94 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat May  5 11:01:01 2018
-
-@author: harald
-"""
-
 import math as math
 from tkinter import *
-import numpy as np
-import git 
 
-### Window
+### Window configuration 1
+### Setting up the window with the tkinter module
 window = Tk()
 window.title("ISA calculator by Harald Luiks")
 window.configure(background="white")
 
-### Label
+### Labelling before the text entry box
 Label (window,text="The altitude in meters is: ",bg="white", fg="black", font="none 12 bold") .grid(row=1,column=0,sticky=W)
 
-### Altitude entry box
+### Creating a text entry box for the altitude 
 textentry = Entry (window,width=10,bg="white")
 textentry.grid(row=2,column=0,sticky=W)
 
-### Click function intitiates calculation for everything
-def click():
-    entered_text = textentry.get()    # collect text from text entry box
+### return_atm_value function intitiates the calculation, this function is encapsulated in the submit button on line 80
+def return_atm_value():
+    entered_text = textentry.get()                                  # Collect text from entry box
     output.delete(0.0,END)
     alt = float(entered_text)
     
-    if alt > 86000: # Give error if given altitude is higher than 86km
+    if alt > 86000:                                                 # Give error if given altitude is higher than 86km
         output.insert(END,"No results above 86 km")
-    else:
+    else:      
+        #======================================================
+        # Constant, ISA SL values and some lists that we'll use
+        r_gas= 287;                                                 # J/kgK        
+        temp_sl = temp_bot = 273.15+15;                             # K at SL
+        pres_sl = pres_bot = 101325;                                # Pa at SL
+        grav0 = 9.80;                                               # m/s^2 at SL
+        rho_sl = pres_sl / (r_gas*temp_sl);                         # Calculate rho at SL in kg/m3        
+        alt_layer = (0, 11000, 20000, 32000, 47000, 51000, 71000);  # Temperature gradient thresholds
+        t_grad = (-.0065, 0, .001, .0028, 0, -.0028, -.002);        # Temperature gradient in layers        
+        l=0;                                                        # Arbitrary zero
        
-    ####
-    
-      #Constant, ISA SL values and some lists we'll use
-        # Physical constants
-        r_gas= 287           # J/kgK
-        
-        # SL values
-        temp_sl = 273.15+15              # K at SL
-        pres_sl = 101325                 # Pa at SL
-        grav0 = 9.80                     # m/s^2 at SL
-        rho_sl = pres_sl/(r_gas*temp_sl)  #calculate rhoSL in kg/m3
-                
-        # Temp gradient list
-        alt_layer = (0, 11000, 20000, 32000, 47000, 51000, 71000)
-        t_grad = (-.0065, 0, .001, .0028, 0, -.0028, -.002)
-        
-        # Define which layers are thermodynamic and isothermal        
-        l=0                     # Arbitrary zero
-        l_iso = (1,4)           # Iso thermal layers
-        l_grad = (0,2,3,5,6)    # Gradient layers
-     
-    ####   
-        
-        
-        # Calculate which layer the altitude is in and the bottom values for each layer
-        while alt >= alt_layer[l]:
-            
-            # Set SL values as bottom values of layer 
-            if l == 0:           
-                temp_bot = temp_sl
-                pres_bot = pres_sl
-                
-            # Calculate values for the bottom of a layer succeeding a gradient layer
-            elif l-1 in l_grad:     
+        #=====================================================
+        # Calculate Temp and Pressure for preceding atmosphere layer threshold
+        while alt > alt_layer[l+1]:                            
+            if t_grad[l] is not 0:                                  # Gradient layer threshold
                 # Calculate the T and P of the top of last layer
-                temp_top = temp_bot + (alt_layer[l]-alt_layer[l-1]) * t_grad[l-1]
-                pres_top = pres_bot*(temp_top/temp_bot)**(-(grav0)/(r_gas*t_grad[l-1]))
+                temp_top = temp_bot + (alt_layer[l+1]-alt_layer[l]) * t_grad[l]
+                pres_bot = pres_bot * (temp_top/temp_bot)**(-(grav0)/(r_gas*t_grad[l]))
+                temp_bot = temp_top                                 # Reset to bottom value
                 
-                # Set these values as the bottom of the current layer
-                temp_bot = temp_top
-                pres_bot = pres_top
-           
-            # Calculate values for the bottom of a layer succeeding a isothermal layer 
-            elif l-1 in l_iso:          
-                temp_bot = temp_bot
-                pres_top = pres_bot * math.exp( (-1 * (alt_layer[l]-alt_layer[l-1]) * (grav0))/ (r_gas * temp_bot))
-                
-                # Set calculated pressure as bottom of current layer
-                pres_bot=pres_top
-            
-            # Update layer height, to check if the altitude is larger than the next atmosphere threshold
-            l = l + 1  
+            else:                                                   # Isothermal layer threshold
+                pres_bot = pres_bot * math.exp( (-1 * (alt_layer[l+1]-alt_layer[l]) * (grav0))/ (r_gas * temp_bot))               
+            l = l + 1                                               # Update layer height for next iteration
         
-        # The while loop stopped, so we now know that the altitude is in the l-1'th layer
-        
-        # Bring l back to the current layer:
-        l=l-1
-        
-        #Calculate the T and P for altitude in gradient layer
-        if l in l_grad:
-            # Calculate the T and P of the top of last layer
+        #=====================================================
+        # Calculating Temperature, Pressure and Density at given altitude
+ 
+        if t_grad[l] is not 0:                                      #Calculate the T and P for altitude in gradient layer
             temp_alt = temp_bot + (alt-alt_layer[l]) * t_grad[l]
             pres_alt = pres_bot*(temp_alt/temp_bot)**(-(grav0)/(r_gas*t_grad[l]))
         
-        elif l in l_iso:          
+        else:                                                       #Calculate the T and P for altitude in isothermal layer
             temp_alt = temp_bot
             pres_alt = pres_bot * math.exp( (-1 * (alt-alt_layer[l]) * (grav0))/ (r_gas * temp_alt))
         
         # Calculate rho and the ratios
-        rho_alt = (pres_alt) / (r_gas * temp_alt)
-        p_ratio=(pres_alt / pres_sl)* 100
-        rh_ratio=(rho_alt / rho_sl)* 100
+        rho_alt = (pres_alt) / (r_gas * temp_alt)                   # Density calculation at given altitude
+        p_ratio = (pres_alt / pres_sl)* 100                         # Pressure calculation at given altitude
+        rh_ratio = (rho_alt / rho_sl)* 100                          # Density calculation at given altitude
+               
+        #=====================================================
+        # Inserting results in textbox
+        out_T = 'Temperature: {}K ({}C)'.format(round(temp_alt,2),round(temp_alt-273.15,1)) 
+        out_P = 'Pressure: {}Pa ({}% SL)'.format(round(pres_alt,0),round(p_ratio,2))
+        out_R = 'Density: {}kg/m3 ({}% SL)'.format(round(rho_alt,5),round(rh_ratio,2))
+        out_list=[out_T,out_P,out_R]                                # List of the different results
+               
+        for i in range(3):                                          # For loop for appending output into result textbox
+            output.insert(END,out_list[i])
+            output.insert(END,'\n')
         
-        # Results
-        temp = ("Temperature:", round(temp_alt,2),"K  (" ,round(temp_alt-273.15,1),"C )" )
-        pres=("Pressure: ",round(pres_alt,0),"Pa  (",round(p_ratio,2)," % SL)")
-        rho=("Density: ",round(rho_alt,5),"kg/m3  (",round(rh_ratio,2)," % SL)")
-        
-        textout=(temp,pres,rho)
-        out=np.array(textout)
-                      
-        output.insert(END,out)
-
-### Submit button
-Button (window,text = "Submit", width=8, command=click) .grid(row=3, column=0, sticky=W)
-
-
-### Result title
+### Window configuration 2
+# Submit button, this contains the return_atm_value function
+Button (window,text = "Submit", width=8, command = return_atm_value) .grid(row=3, column=0, sticky=W)
+ 
+# Result label
 Label (window, text="Temperature, Pressure and Density at given altitude:",bg="white", fg="black", font="none 12 bold") .grid(row=4,column=0,sticky=W)
 
-
-### Result field
+# Result field
 output = Text (window,width=50,height=6,wrap = WORD, background ="white")
 output.grid(row=5, column=0, columnspan=2, sticky=W)
 
-
-### Exit button and functions
+# Exit button and window closure functions
 def close_window():
-    window.destroy()  
-   
+    window.destroy()     
 Button (window, text="Exit", width=14,command=close_window) .grid(row=6, column=0, sticky=W)
 
-### Open window
-window.mainloop()
+# Run window
+window.mainloop() 
